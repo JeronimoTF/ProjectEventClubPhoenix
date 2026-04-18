0d15a4038ef2ee2d9b4d3965e5673ba34776f25f
Jerónimo <tapiasfigueroa3035@gmail.com>
2026-04-17 23:12:30 -0500

    agregar proyecto

---
+++ app/src/main/java/com/example/projecteventclub/vista_usuario/bienvenida/activity_bienvenida.kt
@@ -0,0 +1,21 @@
+package com.example.projecteventclub.vista_usuario.bienvenida
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.projecteventclub.R
+
+class activity_bienvenida : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_bienvenida)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
