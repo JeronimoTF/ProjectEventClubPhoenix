9e520e34f02e9ad0f7f4de2d3cb5b5294b36602a
Carlos-205 <capp.4321@gmail.com>
2026-04-19 00:23:32 -0500

    commit para crear el fragment de modificar datos de usuario, agregar valores y edicion de estilos

---
+++ app/src/main/java/com/example/projecteventclub/vista_usuario/main/usuarios/fragment_configUser.kt
@@ -0,0 +1,12 @@
+package com.example.projecteventclub.vista_usuario.main.usuarios
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.projecteventclub.R
+
+class fragment_configUser : Fragment() {
+
+}
