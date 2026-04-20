2b3a76038fcddfd1d91effa616d83ce9279e2ac1
Jerónimo <tapiasfigueroa3035@gmail.com>
2026-04-19 23:24:14 -0500

    commit creacion de codigo para camara y google maps y boton de redireccion

--- app/src/main/java/com/example/projecteventclub/vista_usuario/main/admin/activity_adminPrincipal.kt
+++ app/src/main/java/com/example/projecteventclub/vista_usuario/main/admin/activity_adminPrincipal.kt
@@ -1,11 +1,16 @@
 package com.example.projecteventclub.vista_usuario.main.admin
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.projecteventclub.R
+import com.example.projecteventclub.atributos.eventos.crear_evento.activity_crearEvento
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,7 +40,43 @@ class activity_adminPrincipal : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.frag_main_adm, container, false)
+        val view = inflater.inflate(R.layout.frag_main_adm, container, false)
+
+        // Botón Registrar Asistencia (Abre Cámara)
+
+        val btnRegistroAsis = view.findViewById<Button>(R.id.BtnRegistroAsis)
+        btnRegistroAsis?.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
+        }
+
+        // Botón Localizar Evento (Abre Google Maps)
+
+        val btnLocalizarEve = view.findViewById<Button>(R.id.BtnlocalizarEve)
+        btnLocalizarEve?.setOnClickListener {
+            val gmmIntentUri = Uri.parse("google.navigation:q=4.6097,-74.0817")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            startActivity(mapIntent)
+        }
+
+        // Botón Agregar Evento
+
+        val btnAgregarEve = view.findViewById<Button>(R.id.BtnAgregarEve)
+        btnAgregarEve?.setOnClickListener {
+            val intent = Intent(context, activity_crearEvento::class.java)
+            startActivity(intent)
+        }
+
+        // Botón Editar Evento
+
+        val btnEditarEve = view.findViewById<Button>(R.id.BtnEditarEve)
+        btnEditarEve?.setOnClickListener {
+            val intent = Intent(context, activity_crearEvento::class.java)
+            startActivity(intent)
+        }
+
+        return view
     }
 
     companion object {
